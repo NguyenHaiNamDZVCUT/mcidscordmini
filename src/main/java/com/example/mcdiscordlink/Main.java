a20e3c01282782ba0436c4576bf52dc7c6810289
NguyenHaiNamDZVCUT <nhnztx.media@gmail.com>
2025-12-09 20:30:48 +0700

    Add files via upload

---
+++ src/main/java/com/example/mcdiscordlink/Main.java
@@ -0,0 +1,10 @@
+package com.example.mcdiscordlink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("MCDiscordLink enabled!");
+    }
+}
